3634254b873c0b0ffced955d6984aa9320f9a217
Anna Burduli <86165953+BurdoAnn@users.noreply.github.com>
2021-08-11 16:45:22 +0400

    js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let navbarlink = document.getElementById("nav");
-let toggelebuttonburger = document.getElementById("togglebutton");
+let toggelebuttonburger = document.getElementById("toggle-button");
 
 toggelebuttonburger.addEventListener('click', function() {
     navbarlink.classList.add('active');
