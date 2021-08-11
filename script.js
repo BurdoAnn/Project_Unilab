aebe2d712c5daa809e0b19537028c1b83d3c4163
Anna Burduli <86165953+BurdoAnn@users.noreply.github.com>
2021-08-11 16:36:40 +0400

    add-burger-bar

---
+++ script.js
@@ -0,0 +1,10 @@
+let navbarlink = document.getElementById("nav");
+let toggelebuttonburger = document.getElementById("togglebutton");
+
+toggelebuttonburger.addEventListener('click', function() {
+    navbarlink.classList.add('active');
+})
+
+// toggelebuttonburger.addEventListener('click', function() {
+// //     navbarlink.classList.add('active');
+// })
