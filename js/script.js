7be27cbd9ecebe690cab2bcc9f8a28dab25aa120
Anna Burduli <86165953+BurdoAnn@users.noreply.github.com>
2021-08-14 13:34:38 +0400

    add-slider

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let navbarlink = document.getElementById("nav");
+let toggelebuttonburger = document.getElementById("togglebutton");
+
+toggelebuttonburger.addEventListener('click', function() {
+    navbarlink.classList.add('active');
+    toggelebuttonburger.addEventListener('click', function() {
+        navbarlink.classList.remove('active');
+    })
+})
+
+// slider//
+
+$(function() {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: true,
+        arrows: false,
+        adaptiveHeight: true,
+        swipe: false,
+
+
+        //რესფონსივის სტილები
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+})
--- script.js
+++
@@ -1,9 +0,0 @@
-let navbarlink = document.getElementById("nav");
-let toggelebuttonburger = document.getElementById("togglebutton");
-
-toggelebuttonburger.addEventListener('click', function() {
-    navbarlink.classList.add('active');
-    toggelebuttonburger.addEventListener('click', function() {
-        navbarlink.classList.remove('active');
-    })
-})
